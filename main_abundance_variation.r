acbac1cc4c1e3700b5f7a0e3d67fdc5b03eede11
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-12-07 16:07:23 +0100

    Create main_abundance_variation.r

---
+++ main_abundance_variation.r
@@ -0,0 +1,8 @@
+source("functions/fun_generic.r")
+source("functions/fun_abundance_variation.r")
+source("functions/fun_glmmTMB_for_variation.r")
+
+
+vecpackage <- c("glmmTMB","ggplot2","dplyr","data.table","corrplot","beepr","plyr","DHARMa")
+
+load_pack(vecpackage)

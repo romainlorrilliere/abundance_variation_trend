caaf9ef2f30748fb275c4ba8c2ad8a9036af1671
Romain Lorrilliere <romainlorrilliere@gmail.com>
2021-05-07 16:31:12 +0200

    Update main_abundance_variation.r

--- main_abundance_variation.r
+++ main_abundance_variation.r
@@ -3,6 +3,9 @@ source("functions/fun_abundance_variation.r")
 source("functions/fun_glmmTMB_for_variation.r")
 
 
-vecpackage <- c("glmmTMB","ggplot2","dplyr","data.table","corrplot","beepr","plyr","DHARMa")
+vecpackage <- c("glmmTMB","ggplot2","dplyr","data.table","corrplot","beepr","plyr","DHARMa","MASS","arm","data.table","scales","psych")
 
 load_pack(vecpackage)
+
+
+
